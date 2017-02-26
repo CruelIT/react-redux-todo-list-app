663bf220a86c25243425ec93843dfe3efe62a47d
Danil Agafonov <mail@live-notes.ru>
2017-02-26 22:29:58 +0300

    add eslint support

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,56 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: [
-            'react-hot-loader/patch',
-            // activate HMR for React
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: [
+      'react-hot-loader/patch',
+      // activate HMR for React
 
-            'webpack/hot/only-dev-server',
-            // bundle the client for hot reloading
-            // only- means to only hot reload for successful updates
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
 
-            './js/index'
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        publicPath: '/',
-        filename: 'assets/js/[name].bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new HtmlWebpackPlugin({template: 'index.html'}),
+      './js/index'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'assets/js/[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        use: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({template: 'index.html'}),
 
-        new webpack.HotModuleReplacementPlugin(),
-        // enable HMR globally
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
 
-        new webpack.NamedModulesPlugin(),
-        // prints more readable module names in the browser console on HMR updates
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        compress: true,
-        port: 8089,
-        publicPath: '/',
-        hot: true
-    }
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 8089,
+    publicPath: '/',
+    hot: true
+  }
 };
 
 module.exports = config;
