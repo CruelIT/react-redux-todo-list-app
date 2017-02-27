34cb5d8e43c140876ee894007ee7bace51efbbaa
Danil Agafonov <mail@live-notes.ru>
2017-02-27 21:48:46 +0300

    styling footer

--- src/js/components/ToDoFooter/index.jsx
+++ src/js/components/ToDoFooter/index.jsx
@@ -7,11 +7,24 @@ const StyledFooter = styled.footer`
   text-align: center;
   border-top: 1px solid #e6e6e6;
   height: 20px;
+  position: relative;
+  
+  &::before {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: 50px;
+    overflow: hidden;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 8px 0 -3px #f6f6f6, 0 9px 1px -3px rgba(0, 0, 0, 0.2), 0 16px 0 -6px #f6f6f6, 0 17px 2px -6px rgba(0, 0, 0, 0.2);
+  }
 `;
 
 const Count = styled.span`
   float: left;
   text-align: left;
+  position: relative;
 `;
 
 const ClearButton = styled.button`
@@ -29,6 +42,7 @@ const ClearButton = styled.button`
   line-height: 20px;
   text-decoration: none;
   cursor: pointer;
+  position: relative;
   
   &:hover {
     text-decoration: underline;
