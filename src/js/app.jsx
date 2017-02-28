205616b424a641bf72c07fc79ff5cb38a031da2c
Danil Agafonov <mail@live-notes.ru>
2017-02-28 18:46:17 +0300

    add redux: create store and inject Provider component from "react-redux"

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,4 +1,16 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {Provider} from 'react-redux';
+
 import MainLayout from './components/MainLayout';
 
-export default () => (<MainLayout />);
+const App = (props) => (
+  <Provider store={props.store}>
+    <MainLayout />
+  </Provider>
+);
+
+App.propTypes = {
+  store: PropTypes.object.isRequired
+};
+
+export default App;
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -4,21 +4,24 @@ import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 // AppContainer is a necessary wrapper component for HMR
 
+import {configureStore} from './store';
+
 import 'normalize.css/normalize.css';
 import '../styles/main.css';
 
 import App from './app';
 
 const rootEl = document.getElementById('root');
+const store = configureStore();
 
 const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
-      <Component />
+      <Component store={store} />
     </AppContainer>,
     rootEl
   );
-}
+};
 
 render(App);
 if (__DEV__ && module.hot) {
